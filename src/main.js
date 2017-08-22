784152babc25de92ce7108cc64abe039bb35adaf
Diego Aguilar <daafaguilar98@gmail.com>
2017-08-22 17:57:42 -0500

    Se agrego el codigo base de analytics

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import {store} from './store'
-import VuePaginate from 'vue-paginate'
-Vue.use(VuePaginate)
+import VueAnalytics from 'vue-analytics'
+
+Vue.use(VueAnalytics, {
+  id: 'UA-92934137-2',
+  router
+})
 
 Vue.config.productionTip = false
 
