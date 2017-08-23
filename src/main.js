25e5d39d9adb7cfa37237c286b4ce570f5909d34
Diego Aguilar <daafaguilar98@gmail.com>
2017-08-23 08:53:02 -0500

    Subdominio concatenado con el currentPath para detencion del analytics

--- src/main.js
+++ src/main.js
@@ -6,9 +6,24 @@ import router from './router'
 import {store} from './store'
 import VueAnalytics from 'vue-analytics'
 
+  var result = {};
+  var regexParse = new RegExp('([a-z\-0-9]{2,63})\.([a-z\.]{2,5})$');
+  var urlParts = regexParse.exec(window.location.hostname);
+  result.domain = urlParts[1];
+  result.type = urlParts[2];
+  result.subdomain = window.location.hostname.replace(result.domain + '.' + result.type, '').slice(0, -1);;
 Vue.use(VueAnalytics, {
-  id: 'UA-92934137-2',
-  router
+  id: 'UA-92934137-1',
+  router,
+  autoTracking: {
+    pageviewTemplate (route) {
+      return {
+        page: result.subdomain + route.path,
+        title: document.title,
+        location: window.location.href
+      }
+    }
+  }
 })
 
 Vue.config.productionTip = false
@@ -23,3 +38,4 @@ new Vue({
 })
 
 store.commit('calculateTotalCart');
+console.log(this.$router)
