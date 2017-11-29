e7c5348b384e3cb0a913cd381646dfdb70611aa2
Diego Aguilar <daafaguilar98@gmail.com>
2017-11-28 19:02:18 -0500

    Es un conjunto de opciones se utiliza para mostrar las variantes

---
+++ src/components/radio_group.vue
@@ -0,0 +1,62 @@
+<template>
+  <ul class="ko-radio_group">
+    <li :class="{'ko-radio_group_option': true, 'backgroundColorPrincipal': option.selected, 'option-selected': option.selected}"
+        v-for="(option, index) in optionsData"
+        v-on:click="selectOption(option, index)">
+        {{ option.option }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['options', 'index'],
+  data(){
+    return {
+      beforeNode: '',
+      optionsData: this.options,
+      beforeIndexOption: '',
+    }
+  },
+  methods: {
+    selectOption(option, index){
+      if(typeof this.beforeIndexOption == 'number'){
+        let changeOption = this.optionsData[this.beforeIndexOption].selected = false;
+        this.optionsData.splice(index, 1, changeOption)
+      }
+      option.selected = true;
+      this.optionsData.splice(index, 1, option)
+      this.beforeIndexOption = index;
+      this.saveOption(option);
+    },
+    saveOption(option){
+      this.$store.state.beforeCombination.splice(this.index, 1, option.option);
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .ko-radio_group{
+    display: flex;
+    list-style: none;
+  }
+  .ko-radio_group_option{
+    margin: 3px;
+    border: 1px solid #EEE;
+    padding: 5px 8px;
+    background-color: #FFF;
+    cursor: pointer;
+    border-radius: 5px;
+    box-shadow: 1px 1px 4px 0px rgba(0,0,0,0.1);
+    font-size: 15px;
+  }
+  .ko-radio_group_option:hover{
+    box-shadow: 0px 0px 0px 0px rgba(0,0,0,0.0);
+    transition: .2s;
+  }
+  .backgroundColorPrincipal.option-selected{
+    color: #FFF;
+    border: 0px solid #EEE;
+  }
+</style>
